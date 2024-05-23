a02798ad5692233e011d85ab5e4fe774e4a29562
Sumant Shah <sumant.shah@gigacars.com>
2024-05-23 16:07:47 +0530

    background message setup

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,36 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyCA2HEMdOM7FvL4CvtzLHDwEr2QFb1yU-U",
+    authDomain: "pushnotification-55577.firebaseapp.com",
+    projectId: "pushnotification-55577",
+    storageBucket: "pushnotification-55577.appspot.com",
+    messagingSenderId: "528049784101",
+    appId: "1:528049784101:web:d266a094c46277f2f9c6cc"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+      '[firebase-messaging-sw.js] Received background message ',
+      payload
+    );
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: payload.notification.image,
+    };
+  
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
