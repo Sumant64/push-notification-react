558620bf76913ee9d8d9f60e08bf5264df28b701
Sumant Shah <sumant.shah@gigacars.com>
2024-05-23 15:31:56 +0530

    added firebase config, getMessaging , generateToken function, tested by generating token

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { generateToken } from "./notification/firebase";
+
 
 function App() {
+
+  useEffect(() => {
+    generateToken();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
---
+++ src/notification/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCA2HEMdOM7FvL4CvtzLHDwEr2QFb1yU-U",
+    authDomain: "pushnotification-55577.firebaseapp.com",
+    projectId: "pushnotification-55577",
+    storageBucket: "pushnotification-55577.appspot.com",
+    messagingSenderId: "528049784101",
+    appId: "1:528049784101:web:d266a094c46277f2f9c6cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+//grant the notification permission
+export const generateToken = async () => {
+    try {
+        const permission = await Notification.requestPermission();
+        console.log(permission); //granted or denied
+        if(permission === 'granted'){
+            const token = await getToken(messaging, {
+                vapidKey: "BO16VYUDXbZnh3KOBnoUkdFLjErcwrSAzBAedd0ViAKgJ5YwooxecaWeZm6CsH02A0IVlIRiu7WEPqTcejp5Dq8"
+            })
+
+            console.log(token)
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
